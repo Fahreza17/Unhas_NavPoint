19fb7ea1273a1dea76cd7e98ba0075c5ba6c91f8
unknown <indminecraft52@gmail.com>
2023-08-09 15:30:50 +0800

    Home Page Update

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -155,13 +155,54 @@ const Home = () => {
             <Accordion.Item eventKey="2" className="accordion-item">
               <Accordion.Header>UNHAS MAPPING</Accordion.Header>
               <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum.
+                <div className="content-container">
+                  <div className="image-content">
+                    <div className="image-pair">
+                      {/* <img src="gambar1.jpg" alt="Gambar 1" className="gambar-proker" /> */}
+                      <p className="map-unhas">
+                        Salah satu proker KKN kami adalah pengembangan Website Navigation kampus dengan QR code untuk memudahkan mahasiswa mencari ruangan dan titik lokasi di kampus, menjawab masalah kurangnya pengetahuan tentang layout kampus terutama bagi mahasiswa baru.
+                      </p>
+                    </div>
+
+                    <div className="image-pair">
+                      <img src="https://iili.io/Ht8fpTb.md.jpg" alt="Gambar 2"  className="gambar-proker"/>
+
+                      <p className="map-unhas">
+                        Tim KKN kami membagi diri menjadi empat kelompok yang bertugas untuk mendata ruangan-ruangan di berbagai fakultas di kampus. Pengumpulan data dilakukan dengan menggunakan aplikasi Avenza, di mana kami memasukkan informasi berupa nama ruangan, koordinat latitude dan longitude, serta deskripsi lantai dari setiap ruangan. Selama hampir dua minggu, kami mengumpulkan data ini secara komprehensif, juga melengkapi informasi dengan gambar-gambar yang mendukung.
+                      </p>
+                    </div>
+
+                    <div className="image-pair">
+                      <img src="https://iili.io/Ht8fbyu.md.jpg" alt="Gambar 3" className="gambar-proker" />
+                      <p className="map-unhas">
+                        Setelah tahap pengumpulan data selesai, kami melanjutkan ke proses pengolahan data. Semua informasi yang telah terkumpul dari berbagai kelompok disatukan menjadi satu database. Database ini akan menjadi dasar informasi yang akan diintegrasikan ke dalam website yang kami rencanakan.
+                      </p>
+                    </div>
+
+                    <div className="image-pair">
+                      <img src="https://iili.io/HtrWAJ4.md.jpg" alt="Gambar 3" className="gambar-proker" />
+                      <p className="map-unhas">
+                        Langkah selanjutnya adalah proses pembuatan website navigasi. Website ini dirancang dengan fitur-fitur yang telah dijelaskan sebelumnya, seperti penggunaan QR code, navigasi berbasis peta interaktif, dan deskripsi ruangan. Tujuannya adalah agar para pengguna website dapat dengan mudah mengakses informasi lokasi ruangan dan merencanakan rute dengan efisien.
+                      </p>
+                    </div>
+
+                    <div className="image-pair">
+                      <img src="https://iili.io/Htr5hbf.md.jpg" alt="Gambar 3" className="gambar-proker" />
+                      <p className="map-unhas">
+                        Sebagai bagian dari proyek ini, kami merancang spanduk-spanduk yang berisi QR code yang dapat discan. QR code ini akan mengarahkan pengguna ke website navigasi yang telah kami kembangkan. Untuk memastikan efektivitasnya, kami mengidentifikasi titik-titik krusial dan area strategis di dalam fakultas-fakultas yang tepat untuk dipasang QR code. Ini memerlukan kerja sama yang baik dengan pihak fakultas untuk menentukan posisi yang optimal.
+                      </p>
+                    </div>
+
+                    <div className="image-pair">
+                      <img src="gambar3.jpg" alt="Gambar kalo terpasang mi stand QR" className="gambar-proker" />
+                      <p className="map-unhas">
+                        Dengan demikian, proyek ini menggabungkan usaha pengumpulan data yang komprehensif, pengolahan data yang cermat, serta pengembangan website yang inovatif. Dengan adanya spanduk-spanduk QR code dan pemasangan QR code di titik-titik penting, proyek ini diharapkan dapat secara signifikan meningkatkan kemudahan navigasi dan pengenalan ruangan bagi para mahasiswa di dalam lingkungan kampus.
+                      </p>
+                    </div>
+                  </div>
+                  <div class="text-content">
+                  </div>
+                </div>
               </Accordion.Body>
             </Accordion.Item>
 
