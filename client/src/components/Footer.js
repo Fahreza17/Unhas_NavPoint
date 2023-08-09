f24678f76e809639ac437d31d54ec70b64abdeab
unknown <indminecraft52@gmail.com>
2023-08-09 13:10:14 +0800

    Footer Update

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,6 +7,21 @@ const Footer = () => {
   return (
     <footer>
       <div className="footerStyle">
+      <div className="logo-container">
+          <div className="half-rectangle outside">
+            <div className="organizer">
+              <p className="organizer-title">Organized By:</p>
+              <img src="https://iili.io/HtgLkFf.md.png" alt="Unhas Logo" className="logo-small" />
+              <img src="https://iili.io/HtgDBAQ.md.png" alt="Smart Campus Logo" className="logo-small" />
+            </div>
+          </div>
+          <div className="half-rectangle inside">
+            <div className="sponsor">
+              <p className="sponsor-title">Sponsored By:</p>
+              <img src="https://iili.io/Htr3xkX.md.png" alt="Simas Logo" className="logo-simas" />
+            </div>
+          </div>
+        </div>
         <div className="socialIconsStyle">
           <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faFacebook} className="iconStyle" />
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -141,26 +141,14 @@ const Home = () => {
                 <div className="text-center">Mental Health Festival</div>
               </Accordion.Header>
               <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum.
+                COOMING SOON
               </Accordion.Body>
             </Accordion.Item>
 
             <Accordion.Item eventKey="1" className="accordion-item">
               <Accordion.Header>SDGS UNHAS</Accordion.Header>
               <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum.
+                COOMING SOON
               </Accordion.Body>
             </Accordion.Item>
 
@@ -180,6 +168,8 @@ const Home = () => {
           </Accordion>
         </Row>
       </Container>
+
+
       <Footer />
     </div>
   );
